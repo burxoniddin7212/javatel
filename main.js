831cf74332be38835707b19f714a3d7c3015009c
burxoniddin7212 <liverpul7212@gmail.com>
2022-04-12 12:50:45 +0500

    poyga

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ var price=100;
 var yourprice=parseInt(prompt("Mablag'ingizni kiriting: ").trim());
 var text=document.createElement("p");
 var texttext=document.createElement("p");
-
 if(yourprice>=price){
   text.textContent="Marhamat olishingiz mumkin!";
   document.body.appendChild(text);
---
+++ mainpoyga.js
@@ -0,0 +1,11 @@
+var age=18;
+var yourAge=prompt("Marhamat yoshingizni kiriting: ").trim();
+//var text=document.createElement="p";
+if(yourAge>=age){
+  document.body.append("Poygada qatnashishingiz mumkin");
+  console.log("Poygada qatnashishingiz mumkin");
+}
+else{
+  document.body.append("Kotta bolsez kelarsiz");
+  console.log("Kotta bolsez kelarsiz");
+}
