f4eefdf83ebe2c2a46c2d9253972bcce3752c800
burxoniddin7212 <liverpul7212@gmail.com>
2022-04-12 12:24:53 +0500

    o'zgarish

--- main.js
+++ main.js
@@ -5,9 +5,11 @@ var texttext=document.createElement("p");
 
 if(yourprice>=price){
   text.textContent="Marhamat olishingiz mumkin!";
+  document.body.appendChild(text);
   console.log("Marhamat olishingiz mumkin!");
 }
 else{
   texttext.textContent="Pulingiz yetmadi Oylik chiqqanda kelarsiz";
-  console.log("Pulingiz yetmadi Oylik chiqqanda kelarsiz!");
+  document.body.appendChild(texttext);
+  console.log("Pulingiz yetmadi oylik chiqqanda kelarsiz!");
 }
